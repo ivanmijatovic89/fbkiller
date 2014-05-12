3c6d0e80d623db7215f5b3051688c823f3c55dfb
Ivan Mijatovic <ivanmijatovic89@gmail.com>
2014-05-12 19:48:10 +0200

    drugi komentar

--- includes/config.php
+++ includes/config.php
@@ -2,6 +2,7 @@
 ini_set('display_errors',0);
    include './hybridauth/config.php';
    //hellp moto
+   // ovo je drugi kommentar
 $CONF = $TMPL = array();
 $CONF['host'] = 'mysql462.loopia.se';
 $CONF['user'] = 'qrd@m18694';
