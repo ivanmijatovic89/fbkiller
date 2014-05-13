85fa73370d119c20709c082e3c10bb98e56b7b19
Ivan Mijatovic <ivanmijatovic89@gmail.com>
2014-05-13 18:17:27 +0200

    jos jednom config

--- includes/config.php
+++ includes/config.php
@@ -1,45 +1,39 @@
 <?php
 ini_set('display_errors',0);
    include './hybridauth/config.php';
-
 $CONF = $TMPL = array();
-$CONF['host'] = 'localhost';
-$CONF['user'] = 'root';
-$CONF['pass'] = '';
-$CONF['name'] = 'facebookkiller';
+$CONF['host'] = 'mysql462.loopia.se';
+$CONF['user'] = 'qrd@m18694';
+$CONF['pass'] = 'm18694@qrd';
+$CONF['name'] = 'mijatovicivan_com_db_4';
 //$CONF['url'] = 'http://localhost/fbOriginal';
-
 $whitelist = array(
     '127.0.0.1',
     '::1'
 );
 if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
-	// localhost
+// localhost
     $CONF['url'] = 'http://localhost/fbGit';
 }else{
-	//server
-	$CONF['url'] = 'http://qrd.mijatovicivan.com';
+$CONF['url'] = 'http://qrd.mijatovicivan.com';
 }
 
 $CONF['email'] = 'ivanmijatovic89@gmail.com';
-
-
-
 $CONF['theme_path'] = 'themes';
-$action = array('admin'			=> 'admin',
-				'feed'			=> 'feed',
-				'settings'		=> 'settings',
-				'messages'		=> 'messages',
-				'post'			=> 'post',
-				'recover'		=> 'recover',
-				'timeline'		=> 'timeline',
-				'profile'		=> 'profile',
-				'notifications'	=> 'notifications',
-				'search'		=> 'search',
-				'page'			=> 'page',
-				'login-social'   => 'login-social',
-				'choose-username'   => 'choose-username'
-				);
+$action = array('admin'	=> 'admin',
+'feed'	=> 'feed',
+'settings'	=> 'settings',
+'messages'	=> 'messages',
+'post'	=> 'post',
+'recover'	=> 'recover',
+'timeline'	=> 'timeline',
+'profile'	=> 'profile',
+'notifications'	=> 'notifications',
+'search'	=> 'search',
+'page'	=> 'page',
+'login-social' => 'login-social',
+'choose-username' => 'choose-username'
+);
 
 // mijat je faca
 ?>
